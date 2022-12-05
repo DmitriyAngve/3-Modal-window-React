e0cd0ec3219022452224df0688b388c21b81e2b0
Dmitriy Angve <kirirch129@gmail.com>
2022-12-05 15:22:22 +0300

    #

--- src/components/Helpers/Wrapper.js
+++ src/components/Helpers/Wrapper.js
@@ -1,5 +1,5 @@
 const Wrapper = (props) => {
-  return props.children; // chelidren prop holds all the content you're passing between the opening and closing tag of your custom component. It is valid in a component to just return that content which you got between the opening and closing tag
+  return props.children;
 };
 
 export default Wrapper;
